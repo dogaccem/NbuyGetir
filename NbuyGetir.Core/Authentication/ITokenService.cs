a044d385fa0f12a4182956463fe881585a2caac1
dogaccem <dogacisoglu@gmail.com>
2022-01-11 16:13:13 +0300

    Add project files.

---
+++ NbuyGetir.Core/Authentication/ITokenService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NbuyGetir.Core.Authentication
+{
+    public class TokenClaim
+    {
+        public string ClaimType { get; set; }
+        public string ClaimValue { get; set; }
+    }
+
+    public class TokenModel
+    {
+        public string AccessToken { get; set; }
+        public DateTime ExpireDate { get; set; }
+    }
+
+    public interface ITokenService
+    {
+        TokenModel CreateAccessToken(List<TokenClaim> claims);
+        void RevokeAccessToken(string token);
+    }
+}
